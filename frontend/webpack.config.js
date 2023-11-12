8f1a9b34270e4d2873bb179cfee8d2d567fd0495
David Abba <david_abba@outlook.com>
2023-11-12 17:52:01 +0100

    Updated configs for the compiler output files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.tsx',
@@ -16,10 +16,6 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
       },
     ],
   },
