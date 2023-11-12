d1a228364c2852add0de8d822f69b759f2a9d74c
David Abba <david_abba@outlook.com>
2023-11-12 17:25:16 +0100

    Added TSX root component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,8 +19,9 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     port: 8080,
   },
 }
