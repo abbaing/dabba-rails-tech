e61066707149b32c9e8552bff945d3999553602d
David Abba <david_abba@outlook.com>
2023-11-14 21:17:04 +0100

    Added my cart button

--- frontend/src/layout/navbar/index.tsx
+++ frontend/src/layout/navbar/index.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
 
 const Navbar = () => {
-    return <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Tech Test</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+  return (
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          Tech Test
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarsExample04'
+          aria-controls='navbarsExample04'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarsExample04">
-        <ul className="navbar-nav me-auto mb-2 mb-md-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-        </ul>
-        <form role="search">
-          <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-        </form>
+        <div className='collapse navbar-collapse' id='navbarsExample04'>
+          <ul className='navbar-nav me-auto mb-2 mb-md-0'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='#'>
+                Home
+              </a>
+            </li>
+          </ul>
+          <button type='button' className='btn btn-dark'>
+            My Cart
+          </button>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  )
 }
 
 export default Navbar
