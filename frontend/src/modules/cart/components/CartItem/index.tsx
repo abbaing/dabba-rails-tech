7d5f36ff46f2ea6a1b4eec44cef71bafd8fefdd0
David Abba <david_abba@outlook.com>
2023-11-15 22:04:49 +0100

    Replaced CSS by modules in cart component

--- frontend/src/modules/cart/components/CartItem/index.tsx
+++ frontend/src/modules/cart/components/CartItem/index.tsx
@@ -2,7 +2,7 @@ import CartModel from 'models/cartModel'
 import React, { useState } from 'react'
 import cartService from 'services/CartService'
 import { formatCurrency } from 'utils/currencyUtils'
-import './index.css'
+import styles from './index.module.css'
 
 interface Props {
   item: CartModel
@@ -12,7 +12,7 @@ const CartItem = ({ item }: Props) => {
   const [count, setCount] = useState(0)
 
   const oldPriceComponent = (price?: number) => {
-    return <span className='old-price'>{formatCurrency(price)}</span>
+    return <span className={styles['old-price']}>{formatCurrency(price)}</span>
   }
 
   const handleIncrement = () => {
@@ -32,45 +32,48 @@ const CartItem = ({ item }: Props) => {
   const product = item.product
 
   return (
-    <div className='col'>
-      <div className='card mb-3'>
-        <div className='row g-0'>
-          <div className='col-md-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>{product.name}</h5>
-              <p className='card-text'>Code {product.code}</p>
-              <p className='card-text'>
-                <small className='text-body-secondary'>
-                  {product.oldPrice && oldPriceComponent(product.oldPrice)}
-                  <span className='price'>
-                    {' '}
-                    {formatCurrency(product.price)}
-                  </span>
-                </small>
-              </p>
-              <div className='alert alert-primary' role='alert'>
-                {product.promotionMessage}
-              </div>
-            </div>
-          </div>
-          <div className='col-md-4'>
-            <div>
-              <button
-                className='btn btn-outline-secondary'
-                onClick={handleDecrement}
-              >
-                -
-              </button>
-              <span className='cart-counter'>{count}</span>
-              <button
-                className='btn btn-outline-secondary'
-                onClick={handleIncrement}
-              >
-                +
-              </button>
-            </div>
+    <div
+      className={`${styles.container} cart-item d-md-flex justify-content-between`}
+    >
+      <div className='col-md-auto px-3 my-3'>
+        <button className='btn btn-sm btn-outline-secondary'>Remove</button>
+      </div>
+      <div className='col-md-5 px-3 my-3'>
+        <h4>{item.product.name}</h4>
+        <span className={`${styles['column-label']}`}>
+          <strong>Code:</strong> {item.product.code}
+        </span>
+      </div>
+      <div className='col-md-auto px-3 my-3 text-center'>
+        <div className={styles['column-label']}>Quantity</div>
+        <div className={styles['count-input']}>
+          <div>
+            <button
+              className='btn btn-outline-secondary'
+              onClick={handleDecrement}
+            >
+              -
+            </button>
+            <span className={styles['cart-counter']}>{count}</span>
+            <button
+              className='btn btn-outline-secondary'
+              onClick={handleIncrement}
+            >
+              +
+            </button>
           </div>
         </div>
+      </div>
+      <div className='col-md-auto px-3 my-3 text-center'>
+        <div className={styles['column-label']}>Price</div>
+        <span className='text-xl font-weight-medium'>
+          {item.product.oldPrice && oldPriceComponent(item.product.oldPrice)}
+          <span>{formatCurrency(item.product.price)}</span>
+        </span>
+      </div>
+      <div className='col-md-auto px-3 my-3 text-center'>
+        <div className={styles['column-label']}>Subtotal</div>
+        <span className='text-xl font-weight-medium'>$35.00</span>
       </div>
     </div>
   )
--- frontend/src/modules/cart/pages/MyCartPage/index.tsx
+++ frontend/src/modules/cart/pages/MyCartPage/index.tsx
@@ -7,9 +7,9 @@ import './index.css'
 const MyCartPage = (): JSX.Element => {
   return (
     <Layout>
-      <h3>Product Catalog</h3>
+      <h3>My Cart</h3>
       <div className='container my-cart-container'>
-        <div className='row row-cols-md-3 g-4'>
+        <div className='row'>
           {cartStore.items.map((item) => (
             <CartItem key={item.product.id} item={item} />
           ))}
