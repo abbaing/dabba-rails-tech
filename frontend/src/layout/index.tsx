8245cbed13c2995b9bf855da58b52f55416812d8
David Abba <david_abba@outlook.com>
2023-11-15 19:54:24 +0100

    Added generic styles

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from 'layout/navbar'
+import React, { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
 
 interface LayoutProps {
   children: ReactNode
@@ -12,16 +13,18 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <ToastContainer position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light" />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
       <div className='container mt-5'>
         <div>{children}</div>
       </div>
