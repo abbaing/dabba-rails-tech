9b3277a4690be3243ce890cdf8835a8790216434
David Abba <david_abba@outlook.com>
2023-11-16 17:03:06 +0100

    Added checkout button

--- frontend/src/modules/cart/pages/MyCartPage/index.tsx
+++ frontend/src/modules/cart/pages/MyCartPage/index.tsx
@@ -4,8 +4,13 @@ import CartItem from 'modules/cart/components/CartItem/index'
 import React from 'react'
 import cartStore from 'stores/cartStore'
 import './index.css'
+import { toast } from 'react-toastify'
 
 const MyCartPage = observer((): JSX.Element => {
+  const handleCheckout = () => {
+    toast.info('Coming Soon :)')
+  }
+
   return (
     <Layout>
       <h1 className='display-5 pb-3 mb-3 border-bottom'>My cart</h1>
@@ -14,6 +19,13 @@ const MyCartPage = observer((): JSX.Element => {
           {cartStore.items.map((item) => (
             <CartItem key={item.product.id} item={item} />
           ))}
+        </div>
+        <div className='row'>
+          <div className='col-md-6'>
+            <button className='btn btn-primary' onClick={handleCheckout}>
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
