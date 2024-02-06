bd29e45961e6999b71a993d88c0a81b4ed365dcd
David Abba <david_abba@outlook.com>
2024-02-06 19:32:00 +0100

    Updated the product boundary class, moved filter methods to private section

--- backend/app/modules/products/boundaries/products_boundary.rb
+++ backend/app/modules/products/boundaries/products_boundary.rb
@@ -1,9 +1,18 @@
-class ProductsBoundary < ::BaseBoundary
-  def filter_by_active(filtered_entities, active)
-    filtered_entities.where(active: active)
+class ProductsBoundary < BaseBoundary
+
+  attr_reader :company_id
+
+  def initialize(company_id)
+    @company_id = company_id
   end
 
-  def filter_by_company(filtered_entities, company_id)
+  private
+
+  def filter_by_active(filtered_entities)
+    filtered_entities.where(active: true)
+  end
+
+  def filter_by_company(filtered_entities)
     filtered_entities.where(company_id: company_id)
   end
 
