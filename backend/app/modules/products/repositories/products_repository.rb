5c9271fd7139ba6f0ad91f2204283ecf6cdb44e5
David Abba <david_abba@outlook.com>
2024-02-10 02:08:38 +0100

    Updated product repository to implement SQLite repo

--- backend/app/modules/products/repositories/products_repository.rb
+++ backend/app/modules/products/repositories/products_repository.rb
@@ -1,21 +1,5 @@
-class ProductsRepository
-  def read_all
-    entities = boundary.find_all
-
-    result = entities.map do |entity|
-      serialize(entity)
-    end
-
-    result
-  end
-
-  private
-
-  def serialize(product)
-    ProductPresenter.new(product).as_json
-  end
-
-  def boundary
-    ProductsBoundary.new
+class ProductsRepository < SqliteRepository
+  def entity
+    Product
   end
 end
