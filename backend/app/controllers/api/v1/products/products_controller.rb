e51d1fe34788e65c121b170af7913bcb833db4b5
David Abba <david_abba@outlook.com>
2024-02-10 02:32:10 +0100

    Updated products controller with the CRUD actions

--- backend/app/controllers/api/v1/products/products_controller.rb
+++ backend/app/controllers/api/v1/products/products_controller.rb
@@ -2,12 +2,53 @@ module Api
   module V1
     module Products
       class ProductsController < ApplicationController
+
         def index
-          products = repository.read_all
-          render json: products, status: :ok
+          @entities = boundary.find_all
+          render json: entities
+        end
+      
+        def show
+          @entity = boundary.find_by_id(params[:id])
+          render json: @entity
+        end
+      
+        def create
+          @product = create_interactor.call(product_params)
+          render json: @product, status: :created
+        end
+      
+        def update
+          @product = update_interactor.call(params[:id], product_params)
+          render json: @product
+        end
+      
+        def destroy
+          delete_interactor.call(params[:id])
+          head :no_content
         end
       
         private
+
+        def product_params
+          params.require(:product).permit(:product_code, :name, :price)
+        end
+      
+        def boundary
+          Products::Boundaries::ProductsBoundary.new
+        end
+      
+        def create_interactor
+          ProductInteractors::Create.new
+        end
+      
+        def update_interactor
+          ProductInteractors::Update.new
+        end
+      
+        def delete_interactor
+          ProductInteractors::Delete.new
+        end
       
         def repository
           ProductsRepository.new
