d2f2f6c5d6358c5c7ab13f6823990c3dc28b05b6
David Abba <david_abba@outlook.com>
2024-02-18 16:54:41 +0100

    Fixed product boundary specs

--- backend/spec/boundaries/products/products_boundary_spec.rb
+++ backend/spec/boundaries/products/products_boundary_spec.rb
@@ -4,20 +4,22 @@ RSpec.describe Products::ProductsBoundary, type: :boundary do
   let(:company_id) { 1 }
   let(:products_boundary) { described_class.new(company_id) }
 
+  let(:product) { double('Product #1') }
+  let(:products) { double('Products') }
+  
+  before do
+    allow(Product).to receive(:all).and_return(products)
+    allow(products).to receive(:includes).with(:product_rules).and_return(products)
+  end
+
   describe "#find_all" do
     it "returns all products" do
-      products = [double('Product'), double('Product')]
-      allow(Product).to receive(:all).and_return(products)
-
       expect(products_boundary.find_all).to eq(products)
     end
   end
 
   describe "#find_by_id" do
     it 'returns the product with the specified id' do
-      product = double('Product', id: 123)
-      products = [product]
-      allow(Product).to receive(:all).and_return(products)
       allow(products).to receive(:find).with(123).and_return(product)
 
       expect(products_boundary.find_by_id(id: 123)).to eq(product)
